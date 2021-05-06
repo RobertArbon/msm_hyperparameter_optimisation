00a79083091d1bb64a45069343df78b2d9273186
rob arbon <robert.arbon@bristol.ac.uk>
2021-05-06 16:11:43 +0100

    hyperparameter dict now has correct keys

--- score_hyperparameters/setup.py
+++ score_hyperparameters/setup.py
@@ -5,17 +5,19 @@ import numpy as np
 NAME = '1FME'
 INPUT_TRAJ_GLOB = 'Volumes/REA/MD/12FF/DESRES-Trajectory_1FME-*-protein/1FME-*-protein'
 NUM_TRIALS = 5
+BS_SAMPLES = 2
 
 #
 # HYPER-PARAMETERS
 # keep values integers. If you need floats, change the units dict.
+# MDTraj deals with nm no angstroms!
 #
 HP_SPACE = {'feature__value': ['phipsi_dihedrals', 'contacts'],
-            'tica__dims': [1, 10], 'tica__lag': [1, 100],
-            'cluster__n_clusters': [10, 1000], 'cluster__max_iter': [1000, 1000],
+            'tica__dim': [1, 10], 'tica__lag': [1, 100],
+            'cluster__k': [10, 1000], 'cluster__max_iter': [1000, 1000],
             'feature__contacts__center': [30, 150]}
 UNITS = {k: 1 for k in HP_SPACE.keys()}
-UNITS['feature__contacts__center'] = 0.1
+UNITS['feature__contacts__center'] = 0.01
 
 #
 # DATA PARAMETERS
