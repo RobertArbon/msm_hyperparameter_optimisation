9e98f8b3833dd6e4e938b9a930e5841ea222e3b4
rob arbon <robert.arbon@bristol.ac.uk>
2021-05-10 19:40:07 +0100

    if only one choice given, sample hp returns taht value

--- score_hyperparameters/sample_hps.py
+++ score_hyperparameters/sample_hps.py
@@ -15,11 +15,13 @@ np.random.seed(149817)
 def sample_hps(hp_space: Dict[str, List[Union[int, str]]]) -> Dict[str, Union[int, str]]:
     sample = {}
     for k, v in hp_space.items():
-        if isinstance(v[0], float):
+        if len(v) == 1:
+            sample[k] = v[0]
+        elif isinstance(v[0], float):
             sample[k] = np.random.uniform(v[0], v[1])
-        if isinstance(v[0], int):
+        elif isinstance(v[0], int):
             sample[k] = np.random.choice(np.arange(v[0], v[1]+1))
-        if isinstance(v[0], str):
+        elif isinstance(v[0], str):
             sample[k] = np.random.choice(v)
     return sample
 
